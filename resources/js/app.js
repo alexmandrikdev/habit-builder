13e66c54903b062c81694c000af0cf2598e54255
MnDAlex <alexvok12@gmail.com>
2020-12-22 11:54:48 +0100

    Setup eslint and prettier

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,18 @@
-require("./bootstrap");
+require('./bootstrap');
 
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-import Vue from "vue";
-import { BootstrapVue } from "bootstrap-vue";
-import VueRouter from "vue-router";
-import router from "./router";
-import App from "./App.vue";
+import Vue from 'vue';
+import { BootstrapVue } from 'bootstrap-vue';
+import VueRouter from 'vue-router';
+import router from './router';
+import App from './App.vue';
 
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
 
 new Vue({
     router,
-    render: h => h(App)
-}).$mount("#app");
+    render: h => h(App),
+}).$mount('#app');
