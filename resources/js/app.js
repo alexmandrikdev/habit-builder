7cbf1117adbd869c5b8f61785d43fe03cd457683
MnDAlex <alexvok12@gmail.com>
2020-12-28 16:22:06 +0100

    Setup BootstrapVueIcons

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,13 +4,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import Vue from 'vue';
-import { BootstrapVue } from 'bootstrap-vue';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 import VueRouter from 'vue-router';
 import router from './router';
 import store from './store';
 import App from './App.vue';
 
 Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 Vue.use(VueRouter);
 
 new Vue({
