0a51a45066a4b799d4bee6bb199f88f2cbaf7d83
MnDAlex <alexvok12@gmail.com>
2020-12-28 10:17:24 +0100

    Implement authentication checking for router

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,14 +3,37 @@ import Router from 'vue-router';
 import Index from './views/Index.vue';
 import Register from './views/Register.vue';
 import Login from './views/Login.vue';
+import store from './store';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     mode: 'history',
     routes: [
-        { path: '/', component: Index },
+        {
+            path: '/',
+            component: Index,
+            meta: {
+                requiresAuth: true,
+            },
+        },
         { path: '/register', component: Register },
         { path: '/login', component: Login },
     ],
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!store.state.isAuthenticated) {
+            next({
+                path: '/login',
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
+
+export default router;
