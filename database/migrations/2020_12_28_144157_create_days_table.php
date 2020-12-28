731941132cc1839bf314e57b8735ec46fdc6d899
MnDAlex <alexvok12@gmail.com>
2020-12-28 17:36:33 +0100

    Set day and habit_id to unique

--- database/migrations/2020_12_28_144157_create_days_table.php
+++ database/migrations/2020_12_28_144157_create_days_table.php
@@ -15,12 +15,14 @@ class CreateDaysTable extends Migration
     {
         Schema::create('days', function (Blueprint $table) {
             $table->id();
-            $table->unsignedSmallInteger('day')->unique();
+            $table->unsignedSmallInteger('day');
             $table->unsignedBigInteger('habit_id');
             $table->foreign('habit_id')->references('id')->on('habits');
             $table->unsignedSmallInteger('silver_stars')->default(0);
             $table->unsignedSmallInteger('golden_stars')->default(0);
             $table->timestamps();
+
+            $table->unique(['habit_id', 'day']);
         });
     }
 
