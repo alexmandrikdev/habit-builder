84279667def85c377352f8e0c21dee7c9c413f03
MnDAlex <alexvok12@gmail.com>
2020-12-29 16:27:51 +0100

    Improve the stop modal

--- resources/js/views/Habit.vue
+++ resources/js/views/Habit.vue
@@ -27,7 +27,7 @@
             </div>
 
             <template v-else>
-                <h3>
+                <h3 class="mt-4">
                     Goal: {{ habit.goal.goal }} day{{
                         habit.goal.goal != 1 ? 's' : ''
                     }}
@@ -35,17 +35,53 @@
 
                 <timer :goal-date="goalDate"></timer>
 
-                <b-button v-b-modal.stop-modal variant="danger">Stop</b-button>
+                <b-button v-b-modal.stop-modal variant="danger" class="mb-4"
+                    >Stop</b-button
+                >
 
                 <b-modal
                     id="stop-modal"
+                    centered
                     title=""
                     header-bg-variant="danger"
                     header-text-variant="light"
+                    @show="onModalShow"
+                    @hide="onModalHide"
                 >
                     <template #modal-header class="text-center">
                         <h5 class="mx-auto my-0">Are you sure?</h5>
                     </template>
+
+                    <div class="text-center">
+                        <h5>
+                            Your goal is: {{ habit.goal.goal }} day{{
+                                habit.goal.goal != 1 ? 's' : ''
+                            }}.
+                        </h5>
+                        <h5>
+                            Your current state is:
+                            {{ currentState }} day{{
+                                currentState > 1 ? 's' : ''
+                            }}.
+                        </h5>
+                        <h5 v-if="currentState > 0">
+                            You will get
+                            <template v-if="currentState < habit.goal.goal">
+                                1
+                                <b-icon
+                                    icon="star-fill"
+                                    class="silver"
+                                ></b-icon>
+                                for Day {{ currentState }}.
+                            </template>
+                            <template v-else>
+                                1
+                                <b-icon icon="star-fill" class="gold"></b-icon>
+                                for Day {{ currentState }}.</template
+                            >
+                        </h5>
+                    </div>
+
                     <template #modal-footer class="text-center">
                         <b-button
                             class="mx-auto my-0"
@@ -91,6 +127,8 @@ export default {
         return {
             habit: null,
             newGoal: null,
+            currentState: null,
+            currentStateInterval: null,
         };
     },
     computed: {
@@ -125,11 +163,29 @@ export default {
     created() {
         this.fetchData();
     },
+    beforeDestroy() {
+        clearInterval(this.currentStateInterval);
+    },
     methods: {
+        onModalShow() {
+            this.calculateCurrentState();
+
+            this.currentStateInterval = setInterval(
+                () => this.calculateCurrentState(),
+                1000,
+            );
+        },
+        onModalHide() {
+            clearInterval(this.currentStateInterval);
+        },
+        calculateCurrentState() {
+            const diffTime =
+                Date.now() - Date.parse(this.habit.goal.created_at);
+
+            this.currentState = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+        },
         fetchData() {
             axios.get(`/habits/${this.$route.params.id}`).then(res => {
-                // console.log(res.data);
-
                 this.habit = res.data;
             });
         },
