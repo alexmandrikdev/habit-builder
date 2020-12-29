de83e68ef24130563f37fe7a898ea9645221c556
MnDAlex <alexvok12@gmail.com>
2020-12-29 15:26:34 +0100

    Create Timer component

---
+++ resources/js/components/Timer.vue
@@ -0,0 +1,74 @@
+<template>
+    <h5 :class="[remainingTimeIsNegative ? 'text-success' : 'text-danger']">
+        {{ remainingTime }}
+    </h5>
+</template>
+
+<script>
+export default {
+    props: {
+        goalDate: {
+            type: Date,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            interval: null,
+            remainingTime: null,
+            remainingTimeIsNegative: false,
+        };
+    },
+    created() {
+        this.calculateRemainingTime();
+
+        this.interval = setInterval(() => {
+            this.calculateRemainingTime();
+        }, 1000);
+    },
+    beforeDestroy() {
+        clearInterval(this.interval);
+    },
+    methods: {
+        calculateRemainingTime() {
+            let diffTime = this.goalDate - Date.now();
+
+            this.remainingTimeIsNegative = diffTime / (1000 * 60 * 60 * 24) < 0;
+
+            diffTime = Math.abs(diffTime);
+
+            const diffDay = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+            let diffHours = Math.floor(diffTime / (1000 * 60 * 60));
+            let diffMinutes = Math.floor(diffTime / (1000 * 60));
+            let diffSeconds = Math.floor(diffTime / 1000) - diffMinutes * 60;
+
+            diffMinutes -= diffHours * 60;
+            diffHours -= diffDay * 24;
+
+            if (diffHours < 10) {
+                diffHours = '0' + diffHours;
+            }
+
+            if (diffMinutes < 10) {
+                diffMinutes = '0' + diffMinutes;
+            }
+
+            if (diffSeconds < 10) {
+                diffSeconds = '0' + diffSeconds;
+            }
+
+            this.remainingTime = '';
+
+            if (diffDay !== 0) {
+                this.remainingTime += `${diffDay} day${
+                    Math.abs(diffDay) !== 1 ? 's' : ''
+                } `;
+            }
+
+            this.remainingTime += `${diffHours}:${diffMinutes}:${diffSeconds}`;
+        },
+    },
+};
+</script>
+
+<style scoped></style>
