d22b055a2e764e332cb992edf837c117f77abf52
MnDAlex <alexvok12@gmail.com>
2020-12-29 17:50:34 +0100

    Display registration errors

--- resources/js/views/Register.vue
+++ resources/js/views/Register.vue
@@ -18,8 +18,13 @@
                     id="email"
                     v-model="formData.email"
                     type="email"
+                    :state="validate('email')"
                     required
                 ></b-form-input>
+
+                <b-form-invalid-feedback v-if="registrationErrors.email">
+                    {{ registrationErrors.email[0] }}
+                </b-form-invalid-feedback>
             </b-form-group>
 
             <b-form-group label="Password:" label-for="password">
@@ -39,8 +44,13 @@
                     id="password_confirmation"
                     v-model="formData.password_confirmation"
                     type="password"
+                    :state="validate('password')"
                     required
                 ></b-form-input>
+
+                <b-form-invalid-feedback v-if="registrationErrors.password">
+                    {{ registrationErrors.password[0] }}
+                </b-form-invalid-feedback>
             </b-form-group>
 
             <b-button type="submit" variant="primary">Submit</b-button>
@@ -58,17 +68,26 @@ export default {
                 password: '',
                 password_confirmation: '',
             },
+            registrationErrors: {},
         };
     },
     methods: {
+        validate(input) {
+            return this.registrationErrors.hasOwnProperty(input) ? false : null;
+        },
         onSubmit() {
             axios.defaults.baseURL = '/';
 
-            axios.post('/register', this.formData).then(response => {
-                this.$store.commit('setIsAuthenticated', true);
+            axios
+                .post('/register', this.formData)
+                .then(response => {
+                    this.$store.commit('setIsAuthenticated', true);
 
-                this.$router.push('/');
-            });
+                    this.$router.push('/');
+                })
+                .catch(error => {
+                    this.registrationErrors = error.response.data.errors;
+                });
 
             axios.defaults.baseURL = '/api/v1';
         },
