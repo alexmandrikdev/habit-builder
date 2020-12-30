aa923f4ad46e44efdc7e1a44765c70a98bf3fe5a
MnDAlex <alexvok12@gmail.com>
2020-12-30 11:27:13 +0100

    Implement GoalHandler component

--- resources/js/views/Habit.vue
+++ resources/js/views/Habit.vue
@@ -13,89 +13,7 @@
                 />
             </h2>
 
-            <div v-if="!habit.goal" class="mt-5">
-                <h4>Set Your Goal</h4>
-                <b-form @submit.prevent="createGoal">
-                    <b-form-group>
-                        <b-form-input
-                            v-model="newGoal"
-                            class="text-center"
-                            required
-                            placeholder="Day"
-                        ></b-form-input>
-                    </b-form-group>
-                    <b-button type="submit" variant="primary">Set</b-button>
-                </b-form>
-            </div>
-
-            <template v-else>
-                <b-card class="my-4">
-                    <h3>
-                        Goal: {{ habit.goal.goal }} day{{
-                            habit.goal.goal != 1 ? 's' : ''
-                        }}
-                    </h3>
-
-                    <timer class="my-2" :goal-date="goalDate"></timer>
-
-                    <b-button v-b-modal.stop-modal variant="danger"
-                        >Stop</b-button
-                    >
-                </b-card>
-
-                <b-modal
-                    id="stop-modal"
-                    centered
-                    title=""
-                    header-bg-variant="danger"
-                    header-text-variant="light"
-                    @show="onModalShow"
-                    @hide="onModalHide"
-                >
-                    <template #modal-header class="text-center">
-                        <h5 class="mx-auto my-0">Are you sure?</h5>
-                    </template>
-
-                    <div class="text-center">
-                        <h5>
-                            Your goal is: {{ habit.goal.goal }} day{{
-                                habit.goal.goal != 1 ? 's' : ''
-                            }}.
-                        </h5>
-                        <h5>
-                            Your current state is:
-                            {{ currentState }} day{{
-                                currentState > 1 ? 's' : ''
-                            }}.
-                        </h5>
-                        <h5 v-if="currentState > 0">
-                            You will get
-                            <template v-if="currentState < habit.goal.goal">
-                                1
-                                <b-icon
-                                    icon="star-fill"
-                                    class="silver"
-                                ></b-icon>
-                                for Day {{ currentState }}.
-                            </template>
-                            <template v-else>
-                                1
-                                <b-icon icon="star-fill" class="gold"></b-icon>
-                                for Day {{ currentState }}.</template
-                            >
-                        </h5>
-                    </div>
-
-                    <template #modal-footer class="text-center">
-                        <b-button
-                            class="mx-auto my-0"
-                            variant="danger"
-                            @click="finishGoal"
-                            >Yes</b-button
-                        >
-                    </template>
-                </b-modal>
-            </template>
+            <goal-handler :fetch-data="fetchData" />
 
             <template v-if="habit.days.length">
                 <b-card v-for="day in weekDays" :key="day.day" class="my-2">
@@ -123,30 +41,18 @@
 </template>
 
 <script>
-import Timer from '../components/Timer.vue';
+import GoalHandler from '../components/GoalHandler.vue';
 import { mapState } from 'vuex';
 
 export default {
-    components: { Timer },
+    components: { GoalHandler },
     data() {
         return {
             loading: false,
-            newGoal: null,
-            currentState: null,
-            currentStateInterval: null,
         };
     },
     computed: {
         ...mapState(['habit']),
-        goalDate() {
-            const goalDate = new Date(this.habit.goal.created_at);
-
-            const day = goalDate.getDate();
-
-            goalDate.setDate(day + parseInt(this.habit.goal.goal));
-
-            return goalDate;
-        },
         week() {
             return this.$route.query.week || 1;
         },
@@ -169,30 +75,10 @@ export default {
     created() {
         this.fetchData();
     },
-    beforeDestroy() {
-        clearInterval(this.currentStateInterval);
-    },
     destroyed() {
         this.$store.commit('setHabit', null);
     },
     methods: {
-        onModalShow() {
-            this.calculateCurrentState();
-
-            this.currentStateInterval = setInterval(
-                () => this.calculateCurrentState(),
-                1000,
-            );
-        },
-        onModalHide() {
-            clearInterval(this.currentStateInterval);
-        },
-        calculateCurrentState() {
-            const diffTime =
-                Date.now() - Date.parse(this.habit.goal.created_at);
-
-            this.currentState = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-        },
         fetchData() {
             this.loading = true;
 
@@ -201,27 +87,6 @@ export default {
 
                 this.loading = false;
             });
-        },
-        createGoal() {
-            axios
-                .post('/goals', {
-                    goal: this.newGoal,
-                    habit_id: this.habit.id,
-                })
-                .then(res => {
-                    this.newGoal = null;
-
-                    this.$store.commit('setHabitGoal', res.data);
-                });
-        },
-        finishGoal() {
-            axios
-                .put(`/goals/${this.habit.goal.id}`, {
-                    habit_id: this.habit.id,
-                })
-                .then(res => {
-                    this.fetchData();
-                });
         },
         handleWeekChange(week) {
             this.$router.push({
